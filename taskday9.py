21c08e4cee0c0b714d7554db7300d5c808251417
Hadi-ameen-pk <hadiameen166@gmail.com>
2025-08-05 10:23:22 +0530

    first commit

--- taskday9.py
+++ taskday9.py
@@ -2,3 +2,13 @@ total = 0
 for i in range(1, 11):
     total += i
 print("Sum:", total)
+
+numbers = [1, 2, 3, 4, 5, 6]
+for num in numbers:
+    if num % 2 == 0:
+        print(num)
+
+for i in range(1, 11):
+    for j in range(1, 6):
+        print(f"{i*j:3}", end=" ")
+    print()
