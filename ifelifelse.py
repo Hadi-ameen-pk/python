513ff2fdfe147f65703cd8856995322602e328a6
Hadi-ameen-pk <hadiameen166@gmail.com>
2025-08-05 10:31:51 +0530

    first commit

--- ifelifelse.py
+++ ifelifelse.py
@@ -5,3 +5,11 @@ elif score >= 50:
     print("Passed")
 else:
     print("Failed")
+
+num=int(input("Enter a number: "))
+if num > 0:
+    print(num, "is positive")
+elif num == 0:
+    print(num, "is zero")
+else:
+    print(num, "is negative")
