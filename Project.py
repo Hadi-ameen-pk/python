d3449d6624961d77c92503b42e4934b182708c53
Hadi-ameen-pk <hadiameen166@gmail.com>
2025-08-12 12:02:36 +0530

    first commit

---
+++ Project.py
@@ -0,0 +1,16 @@
+id=1
+my_tasks={}
+def AddTask(n):
+    global my_tasks
+    global id
+    for t in range(n):
+        task=input("Enter the task: ")
+        my_tasks[id] = {"tasks"}
+        id+=1
+    print(my_tasks)
+print("ToDo Tasks")
+print("1.Add Task\n2.Update Task\n3.Remove Task\n4.Show Task")
+choice=int(input("Enter your choice:"))
+if choice==1:
+
+    AddTask(task_count)
