037159466f3ac6baa8d62fbe29741770d9944b99
Hadi-ameen-pk <hadiameen166@gmail.com>
2025-09-15 11:06:18 +0530

    first commit

--- brangingcode.py
+++ brangingcode.py
@@ -198,3 +198,17 @@ result = MathOperations.add(4, 6)   # sum = 10
 print("Sum:", result)
 print(MathOperations.check_even_odd(result))
 '''
+
+
+# List of favorite foods
+favorite_foods = ["Pizza", "Biryani", "Burger", "Pasta", "Ice Cream"]
+
+# Using for loop to print
+for food in favorite_foods:
+    print(f"I love eating {food}.")
+# List of favorite foods
+favorite_foods = ["Pizza", "Biryani", "Burger", "Pasta", "Ice Cream"]
+
+# Using for loop to print
+for food in favorite_foods:
+    print(f"I love eating {food}.")
---
+++ task15.py
@@ -0,0 +1,3 @@
+foods = ["Pizza", "Biryani", "Burger", "Pasta", "Ice Cream"]
+for food in foods:
+    print(f"I love eating {food}.")
